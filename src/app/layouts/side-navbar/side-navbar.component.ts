7fd51caae9caa9c6829375dc25bf14083fe3a6c0
Ma Seo Sen | Tub Xyaas Muas <pyhteam.solutions@gmail.com>
2024-09-17 11:26:32 +0700

    add theme

--- src/app/layouts/side-navbar/side-navbar.component.ts
+++ src/app/layouts/side-navbar/side-navbar.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-side-navbar',
   templateUrl: './side-navbar.component.html',
-  styleUrl: './side-navbar.component.scss'
+  styleUrl: './side-navbar.component.scss',
 })
 export class SideNavbarComponent {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Documents',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-plus',
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-search',
+          },
+        ],
+      },
+      {
+        label: 'Profile',
+        items: [
+          {
+            label: 'Settings',
+            icon: 'pi pi-cog',
+          },
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+          },
+        ],
+      },
+    ];
+  }
 }
--- src/app/shared/prime-ng.module.ts
+++ src/app/shared/prime-ng.module.ts
@@ -7,28 +7,27 @@ import { AvatarModule } from 'primeng/avatar';
 import { InputTextModule } from 'primeng/inputtext';
 import { RippleModule } from 'primeng/ripple';
 import { AvatarGroupModule } from 'primeng/avatargroup';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { MenuModule } from 'primeng/menu';
 
+const primeNgModules = [
+  MenubarModule,
+  BadgeModule,
+  AvatarModule,
+  InputTextModule,
+  RippleModule,
+  AvatarGroupModule,
+  MegaMenuModule,
+  ButtonModule,
+  ToastModule,
+  MenuModule,
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-
-    MenubarModule,
-    BadgeModule,
-    AvatarModule,
-    InputTextModule,
-    RippleModule,
-    AvatarGroupModule,
-
-  ],
-  exports: [
-    MenubarModule,
-    BadgeModule,
-    AvatarModule,
-    InputTextModule,
-    RippleModule,
-    AvatarGroupModule,
-  ],
+  imports: [CommonModule, ...primeNgModules],
+  exports: [...primeNgModules],
 })
-export class PrimeNgModule { }
+export class PrimeNgModule {}
